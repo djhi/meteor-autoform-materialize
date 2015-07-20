839ed27110accbf6ba94d33be704283c92cfe20f
Lance Ennen <byennen@gmail.com>
2015-07-20 13:48:57 -0400

    fix checkbox value #49 borrowed from @kamek-pf comment

--- inputTypes/select-checkbox-inline/select-checkbox-inline.js
+++ inputTypes/select-checkbox-inline/select-checkbox-inline.js
@@ -2,3 +2,13 @@ Template.afCheckboxGroupInline_materialize.helpers({
     dsk:      Utility.helpers.dsk,
     itemAtts: Utility.helpers.itemAttsWithUniqId
 });
+
+Template.afCheckboxGroupInline_materialize.onRendered(function () {
+    var template = this;
+    if (this.data.value) {
+        this.data.value.forEach(function (value) {
+            var selector = 'input[value="' + value + '"]';
+            template.$(selector).prop('checked', true);
+        });
+    }
+});
