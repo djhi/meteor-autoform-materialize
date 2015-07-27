c9e4ace528111f7227a9e71ea0e05de78cfe68be
Lance Ennen <byennen@gmail.com>
2015-07-27 09:39:20 -0400

    onRendered select radio button that should be selected

--- inputTypes/select-radio-inline/select-radio-inline.js
+++ inputTypes/select-radio-inline/select-radio-inline.js
@@ -2,3 +2,11 @@ Template.afRadioGroupInline_materialize.helpers({
   dsk:      Utility.helpers.dsk,
   itemAtts: Utility.helpers.itemAttsWithUniqId
 });
+
+Template.afRadioGroupInline_materialize.onRendered(function () {
+    var template = this;
+    if (this.data.value) {
+      var selector = 'input[value="' + this.data.value + '"]';
+      template.$(selector).prop('checked', true);
+    }
+});
--- inputTypes/select-radio/select-radio.js
+++ inputTypes/select-radio/select-radio.js
@@ -2,3 +2,11 @@ Template.afRadioGroup_materialize.helpers({
   dsk:      Utility.helpers.dsk,
   itemAtts: Utility.helpers.itemAttsWithUniqId
 });
+
+Template.afRadioGroup_materialize.onRendered(function () {
+    var template = this;
+    if (this.data.value) {
+      var selector = 'input[value="' + this.data.value + '"]';
+      template.$(selector).prop('checked', true);
+    }
+});
