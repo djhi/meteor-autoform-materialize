773f9d2d99c849a47967c2f2aa99960e85df7341
Chun Yang <yang2007chun@gmail.com>
2015-09-08 22:31:50 -0500

    remove unnecessary stuff

--- inputTypes/range/range.js
+++
@@ -1,3 +0,0 @@
-Template.afInputRange_materialize.helpers({
-  attrs: Utility.attsToggleInvalidClass
-})
--- inputTypes/text/text.js
+++ inputTypes/text/text.js
@@ -1,7 +1,3 @@
 Template.afInputText_materialize.helpers({
   attrs: Utility.attsToggleInvalidClass
-});
-
-Template.afInputText_materialize.rendered = function() {
-    this.$('textarea').characterCounter();
-}
+})
