10101aba0b02e25fb9e6d6ff52d320365ba8e4df
Chun Yang <yang2007chun@gmail.com>
2015-09-10 14:00:00 -0500

    Revert "Fixes #55, #49"
    This reverts commit 6b5ba5316672af80313b1a452233dd07c339c613.

--- inputTypes/select-checkbox-inline/select-checkbox-inline.js
+++ inputTypes/select-checkbox-inline/select-checkbox-inline.js
@@ -1,7 +1,4 @@
 Template.afCheckboxGroupInline_materialize.helpers({
     dsk:      Utility.helpers.dsk,
-    itemAtts: function() {
-        var atts = Utility.helpers.itemAttsWithUniqId(this);
-        return Utility.helpers.attsCheckSelected(this);
-    }
+    itemAtts: Utility.helpers.itemAttsWithUniqId
 });
--- inputTypes/select-radio-inline/select-radio-inline.js
+++ inputTypes/select-radio-inline/select-radio-inline.js
@@ -1,7 +1,4 @@
 Template.afRadioGroupInline_materialize.helpers({
   dsk:      Utility.helpers.dsk,
-  itemAtts: function() {
-      var atts = Utility.helpers.itemAttsWithUniqId(this);
-      return Utility.helpers.attsCheckSelected(this);
-  }
+  itemAtts: Utility.helpers.itemAttsWithUniqId
 });
