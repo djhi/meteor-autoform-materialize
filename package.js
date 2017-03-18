d4333064239cdbe6c856fc3583223d44a6952509
Wynand <groendrakie@gmail.com>
2017-03-18 19:45:05 +0200

    updated package and repo info

--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
-  name: "gildaspk:autoform-materialize",
+  name: "mozfet:autoform-materialize",
   summary: "Materialize theme for Autoform",
   version: "0.0.26",
-  git: "https://github.com/djhi/meteor-autoform-materialize.git"
+  git: "https://github.com/mozfet/meteor-autoform-materialize.git"
 })
 
 Package.onUse(function(api) {
