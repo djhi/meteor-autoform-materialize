1481a86add2fc19ce8207302259063b22e0d62de
Wynand <groendrakie@gmail.com>
2017-03-29 13:49:09 +0200

    added firstOption support and improved styling

--- inputTypes/select-multiple/select-multiple.js
+++ inputTypes/select-multiple/select-multiple.js
@@ -1,10 +1,30 @@
-Template.afSelectMultiple_materialize.helpers({
-  optionAtts: Utility.optionAtts
+/*jshint esversion: 6 */
+
+Template.afSelectMultiple_material.onRendered(function() {
+  //init select
+  $('select').material_select();
+
+  //get select wrapper
+  const select = $('#'+this.data.atts.id);
+  const selectWrapper = select.parent();
+
+  //if no value is defined and firstOption is defined
+  if ((this.data.value.length===0) && (this.data.atts.firstOption)) {
+
+    //initialise value
+    this.value.set(this.data.atts.firstOption);
+    const input = selectWrapper.find('input.select-dropdown:first');
+    input.attr('value', this.data.atts.firstOption);
+  }
 });
 
-Template.afSelectMultiple_materialize.helpers({
-  atts: function() {
-    var atts = Utility.attsToggleInvalidClass.call(this);
-    return AutoForm.Utility.addClass(atts, 'browser-default');
+Template.afSelectMultiple_material.helpers({
+  optionAtts: Utility.optionAtts,
+  atts: Utility.attsToggleInvalidClass.call(this),
+  firstValueSelected: function () {
+    if ((this.value.length===0) && (this.data.atts.firstOption)) {
+      return true;
+    }
+    return false;
   }
 });
